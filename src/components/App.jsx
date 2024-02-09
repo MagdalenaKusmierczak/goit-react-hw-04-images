f24ab41c9e1f7514f78e7f450cd57e6c36874447
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-02-09 14:09:17 +0100

    finish hooks implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -6,143 +6,109 @@ import MoreButton from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    inputValue: '',
-    images: [],
-    currentPage: 1,
-    error: null,
-    isLoading: false,
-    totalPages: 0,
-    isModalOpen: false,
-    modalImage: '',
-    modalTags: '',
-  };
-  //API call
-  fetchImages = async (inputValue, currentPage) => {
-    const API_KEY = '35262306-2ee6f92f6616bfcf6c7291f6d';
-    axios.defaults.baseURL = 'https://pixabay.com/api/';
-    const perPage = 12;
-    const response = await axios.get(
-      `?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${perPage}`
-    );
-    return response.data;
-  };
-
-  imagesData = responseData =>
-    responseData.map(({ id, tags, webformatURL, largeImageURL }) => {
-      return { id, tags, webformatURL, largeImageURL };
-    });
+export const App = () => {
+  //Declaration
+  const [inputValue, setInputValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+  const [modalTags, setModalTags] = useState('');
 
   //Load more button
-  loadMore = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    }));
+  const loadMore = () => {
+    setCurrentPage(prevPage => prevPage + 1);
   };
   //Submit search
-  handleSubmit = input => {
-    this.setState({
-      inputValue: input,
-      images: [],
-      currentPage: 1,
-    });
+  const handleSubmit = input => {
+    setInputValue(input);
+    setImages([]);
+    setCurrentPage(1);
   };
 
-  //Generates gallery
-  addImages = async () => {
-    const { inputValue, currentPage } = this.state;
-    try {
-      this.setState({ isLoading: true });
-      const data = await this.fetchImages(inputValue, currentPage);
-
-      if (data.hits.length === 0) {
-        return alert('Image not found');
+  //Update state
+  useEffect(() => {
+    if (inputValue === '') {
+      return;
+    }
+    //API call
+    const fetchImages = async (inputValue, currentPage) => {
+      const API_KEY = '35262306-2ee6f92f6616bfcf6c7291f6d';
+      axios.defaults.baseURL = 'https://pixabay.com/api/';
+      const perPage = 12;
+      const response = await axios.get(
+        `?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${perPage}`
+      );
+      return response.data;
+    };
+    //Destructuring image data
+    const imagesData = responseData =>
+      responseData.map(({ id, tags, webformatURL, largeImageURL }) => {
+        return { id, tags, webformatURL, largeImageURL };
+      });
+    //Generates gallery
+    const addImages = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetchImages(inputValue, currentPage);
+        if (data.hits.length === 0) {
+          return alert('Image not found');
+        }
+        const normalizedImages = imagesData(data.hits);
+        setImages(prevImages => [...prevImages, ...normalizedImages]);
+        setTotalPages(Math.ceil(data.totalHits / 12));
+        setIsLoading(false);
+      } catch (error) {
+        console.log(`Something went wrong! ${error}`);
+      } finally {
+        setIsLoading(false);
       }
-      const normalizedImages = this.imagesData(data.hits);
+    };
+    addImages();
+  }, [inputValue, currentPage]);
 
-      this.setState(state => ({
-        images: [...state.images, ...normalizedImages],
-        totalPages: Math.ceil(data.totalHits / 12),
-        isLoading: false,
-        error: '',
-      }));
-    } catch (error) {
-      this.setState({ error: 'Something went wrong!' });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.inputValue !== this.state.inputValue ||
-      prevState.currentPage !== this.state.currentPage
-    ) {
-      this.addImages();
-    }
-  }
   //Modal
-  handleClose = evt => {
-    this.setState({
-      isModalOpen: false,
-      modalImage: '',
-      modalTags: '',
-    });
+  const handleClose = () => {
+    setIsModalOpen(false);
+    setModalImage('');
+    setModalTags('');
   };
-  onClose = evt => {
+  const onClose = evt => {
     if (evt.target === evt.currentTarget) {
-      this.handleClose();
+      handleClose();
     }
   };
-  handleClick = evt => {
-    this.setState({
-      isModalOpen: true,
-      modalTags: evt.target.alt,
-      modalImage: evt.target.name,
-    });
+  const handleClick = evt => {
+    setIsModalOpen(true);
+    setModalImage(evt.target.name);
+    setModalTags(evt.target.alt);
   };
-  async componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  async componentWillUnmount() {
-    window.removeEventListener('keydown', this.onClose);
-  }
-
-  handleKeyDown = evt => {
+  const handleKeyDown = evt => {
     if (evt.code === 'Escape') {
-      this.handleClose();
+      handleClose();
     }
   };
-
-  render() {
-    const {
-      images,
-      isLoading,
-      totalPages,
-      currentPage,
-      isModalOpen,
-      modalImage,
-      modalTags,
-    } = this.state;
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleSubmit} />
-        {images.length > 0 && (
-          <ImageGallery imageClick={this.handleClick} images={images} />
-        )}
-        {isLoading && <Loader />}
-        {images.length > 0 && totalPages !== currentPage && (
-          <MoreButton onClick={this.loadMore} />
-        )}
-        {isModalOpen ? (
-          <Modal
-            largeImageURL={modalImage}
-            tags={modalTags}
-            onClose={this.onClose}
-          />
-        ) : null}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', onClose);
+    };
+  });
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleSubmit} />
+      {images.length > 0 && (
+        <ImageGallery imageClick={handleClick} images={images} />
+      )}
+      {isLoading && <Loader />}
+      {images.length > 0 && totalPages !== currentPage && (
+        <MoreButton onClick={loadMore} />
+      )}
+      {isModalOpen ? (
+        <Modal largeImageURL={modalImage} tags={modalTags} onClose={onClose} />
+      ) : null}
+    </div>
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,44 +1,40 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-class Searchbar extends Component {
-  state = {
-    inputValue: '',
-    search: '',
+const Searchbar = ({ onSubmit }) => {
+  const [search, setSearch] = useState('');
+
+  const handleInput = evt => {
+    setSearch(evt.target.value);
   };
-  handleInput = evt => {
-    this.setState({ search: evt.target.value });
-  };
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
     const trimInput = evt.target.elements.inputValue.value.trim();
-    this.props.onSubmit(trimInput);
+    onSubmit(trimInput);
     evt.target.reset();
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            name="inputValue"
-            id="search"
-            placeholder="Search images and photos"
-            value={this.state.search}
-            onChange={this.handleInput}
-          />
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          name="inputValue"
+          id="search"
+          placeholder="Search images and photos"
+          value={search}
+          onChange={handleInput}
+        />
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
+      </form>
+    </header>
+  );
+};
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
 };
